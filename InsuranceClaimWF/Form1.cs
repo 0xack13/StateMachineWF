07890fac2bdea3a68050a7f3b6c4f26f8c5cf800
0xack13 <0xack13@gmail.com>
2014-12-01 17:22:45 +0300

    Workflow Initiation code

--- InsuranceClaimWF/Form1.cs
+++ InsuranceClaimWF/Form1.cs
@@ -12,9 +12,25 @@ namespace InsuranceClaimWF
 {
     public partial class Form1 : Form
     {
+        WorkflowRuntime workflowRuntime = null;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            workflowRuntime = new WorkflowRuntime();
+            workflowRuntime.StartRuntime();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //workflowRuntime = new WorkflowRuntime();
+            workflowRuntime.StopRuntime();
+
+        }
+
     }
 }
